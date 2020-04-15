82264148d553a4efb5aea4ef5887918a41b4ce5b
sunyuting <sleepsun123@163.com>
2020-04-15 11:59:09 +0800

    add marquee to notice

--- src/components/Notice.jsx
+++ src/components/Notice.jsx
@@ -8,6 +8,34 @@ export default class Notice extends Component {
     }
   }
 
+  startmarquee(lh,speed,delay) {
+		var p=false;
+		var t;
+		var o=this._marquee;
+		o.innerHTML+=o.innerHTML;
+		o.style.marginTop=0;
+
+		function start(){
+			t=setInterval(scrolling,speed);
+			if(!p) o.style.marginTop=parseInt(o.style.marginTop)-1+"px";
+		}
+
+		function scrolling(){
+			if(parseInt(o.style.marginTop)%lh!=0){
+				o.style.marginTop=parseInt(o.style.marginTop)-1+"px";
+				if(Math.abs(parseInt(o.style.marginTop))>=o.scrollHeight/2) o.style.marginTop=0;
+			}else{
+				clearInterval(t);
+				setTimeout(start,delay);
+			}
+		}
+		setTimeout(start,delay);
+	}
+  componentDidMount(){
+    let marHeight = this._marquee.childNodes[0].clientHeight
+    if(this.props.data.length > 0) this.startmarquee(marHeight,marHeight,3500);
+  }
+
   render() {
     const notice = this.state.notice
     return (
@@ -15,7 +43,12 @@ export default class Notice extends Component {
         <div class="row">
           <div class="col col-20 one-news"><b>网站</b><span>公告</span></div>
           <div class="col col-80 marqueebox">
-            <span>{notice}</span>
+            <div ref={c => this._marquee = c}>
+              {notice && notice.length > 0 ? notice.map((x,i)=> 
+                <span key={i}>{x}</span>
+              ):
+              <span>还没有公告</span>}
+            </div>
           </div>
         </div>
       </div>
